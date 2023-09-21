d025c2689748d74aed3f64555bbea2acbc5e4dd4
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-09-21 17:03:59 -0400

    feat: add MapUtil

---
+++ src/main/java/util/MapUtil.java
@@ -0,0 +1,44 @@
+package util;
+
+public class MapUtil implements IMapUtil{
+
+    /**
+     *
+     */
+    @Override
+    public void loadMap() {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void editMap() {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void saveMap() {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void validateMap() {
+
+    }
+
+    /**
+     *
+     */
+    @Override
+    public void showMap() {
+
+    }
+}
