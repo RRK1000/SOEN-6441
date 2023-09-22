32b92a5ffaa692bdcdbb9c6b9783c500683f97f5
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-09-22 14:29:34 -0400

    feat: update loadMap()

--- src/test/java/util/MapUtilTest.java
+++ src/test/java/util/MapUtilTest.java
@@ -1,21 +1,32 @@
 package util;
 
+import models.Country;
 import models.Map;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 class MapUtilTest {
-    private Map map;
+    private Map d_map;
 
     @BeforeEach
     void setUp() {
-        map = new Map();
+        d_map = new Map();
     }
 
     @Test
     void loadMap()  {
         MapUtil mapUtil = new MapUtil();
-        mapUtil.map = map;
+        mapUtil.d_map = d_map;
         mapUtil.loadMap("src/test/resources/validMap1.txt");
+
+        for(Country l_c : d_map.getD_countryMapGraph().vertexSet()){
+            System.out.println(d_map.getD_countryMapGraph().outgoingEdgesOf(l_c));
+        }
+
+        assertFalse(d_map.getD_countryMapGraph().vertexSet().isEmpty());
+        assertFalse(d_map.getD_continentMapGraph().vertexSet().isEmpty());
+
     }
 }
