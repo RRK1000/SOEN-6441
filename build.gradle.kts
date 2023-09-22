9e6f7ad1dcf4274bb5d1583f8831ad384f3b18f0
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-09-22 19:53:00 -0400

    refactor: reformat code + update jgrapht version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jgrapht:jgrapht-core:1.5.2")
+    implementation("org.jgrapht:jgrapht-core:1.4.0")
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
