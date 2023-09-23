5898252a9fff49bab340f1bebfbddc3b000ddd02
Nimisha Jadav <nimisha.jadav7@gmail.com>
2023-09-23 16:52:49 -0400

    feat: added functions in GameStartup
    feat: added functions in GameStartup

--- src/main/java/controller/GameStartup.java
+++ src/main/java/controller/GameStartup.java
@@ -1,5 +1,11 @@
 package controller;
 
+import java.io.FileReader;
+import java.util.*;
+import java.io.*;
+import util.MapUtil;
+import models.Map;
+
 public class GameStartup {
 
     private int d_gameplayer;
@@ -12,15 +18,45 @@ public class GameStartup {
         return d_gameplayer;
     }
 
-    public void loadmap(){
+    public void assignCountries(){
 
     }
 
-    public void addPlayerName(){
-
+    public int menuGame(){
+        int d_choice;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1. Continue Existing Game");
+        System.out.println("2. Start a New Game");
+        System.out.println("3. Exit");
+        System.out.println("Please enter your choice : ");
+        d_choice = sc.nextInt();
+        return d_choice;
     }
 
-    public void removePlayerName(){
+    public void loadGame(){
 
+        Scanner sc = new Scanner(System.in);
+        Map d_map;
+        String d_mapName;
+        System.out.println("Enter the map name to load the game");
+        d_mapName = sc.next();
+        //write logic to check whether the map exist or not, if not create a new map
+        if(ifMapExists(d_mapName)){
+            //continue the saved game
+        }
+        else{
+            //start a new game
+        }
     }
+
+    public void newGame(){
+        //write logic to create new map and then validate the map
+    }
+
+    public boolean ifMapExists(String d_mapName){
+        File f_file = new File(d_mapName);
+        boolean d_exists = f_file.exists();
+        return d_exists;
+    }
+
 }
