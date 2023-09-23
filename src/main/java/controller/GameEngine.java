4648f95a07a0b0229d080549dab640ad73ef8b0a
Nimisha Jadav <nimisha.jadav7@gmail.com>
2023-09-23 01:11:30 -0400

    feat: added gameMenu() functionality
    feat: added gameMenu() functionality

--- src/main/java/controller/GameEngine.java
+++ src/main/java/controller/GameEngine.java
@@ -3,8 +3,32 @@ package controller;
 public class GameEngine {
 
     public static void main(String[] p_args){
+        System.out.println("WELCOME TO WARZONE");
 
+        System.out.println("---GAME BEGINS---");
 
+        gameMenu();
+    }
+
+    public static void gameMenu(){
+        GameStartup gameStartup = new GameStartup();
+
+        int d_selectMenuOption = gameStartup.menuGame();
+        do{
+            switch (d_selectMenuOption){
+                case 1:
+                    gameStartup.loadGame();
+                    break;
+                case 2:
+                    gameStartup.newGame();
+                    break;
+                case 3:
+                    System.out.println("Exiting from the game");
+                    break;
+                default:
+                    System.out.println("Incorrect Input! Enter the correct option");
+            }
+        }while(d_selectMenuOption!=3);
     }
 
 }
