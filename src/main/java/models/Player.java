13e4a3d2dc67afced0fddf98707b75ae88878701
Anuja-Somthankar <anujasom@gmail.com>
2023-09-27 17:18:29 -0400

    feat : add Order class and modifications to Player class
    feat : add Order class and modifications to Player class

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -1,15 +1,21 @@
 package models;
 
+import java.util.List;
+
 /**
  * Represents a Player in the game.
  * Manages player-related functionalities.
  * This class is used in relationship with the GameController class to manage player actions.
  *
- * @author Yusuke
+ * @author Yusuke, Anuja-Somthankar
  */
 public class Player {
     // Data Members
     private String d_playerName;
+    private int d_numArmies;
+    private List<Country> d_countryList;
+    private List<Order> d_orderList;
+
 
     /**
      * Default constructor for Player class.
@@ -18,13 +24,11 @@ public class Player {
         // Implementation here
     }
 
-    /**
-     * Constructor with parameters for Player class.
-     *
-     * @param p_playerName The name of the player.
-     */
-    public Player(String p_playerName) {
+    public Player(String p_playerName, int p_numArmies, List<Country> p_countryList, List<Order> p_orderList) {
         this.d_playerName = p_playerName;
+        this.d_numArmies = p_numArmies;
+        this.d_countryList = p_countryList;
+        this.d_orderList = p_orderList;
     }
 
     /**
@@ -32,13 +36,16 @@ public class Player {
      */
     public void issueOrder() {
         // Implementation here
+
     }
 
     /**
      * Moves to the next order.
      */
-    public void nextOrder() {
+    public Order nextOrder() {
         // Implementation here
+
+        return null;
     }
 
     /**
@@ -57,5 +64,53 @@ public class Player {
      */
     public void setPlayerName(String p_playerName) {
         this.d_playerName = p_playerName;
+    }
+
+    /**
+     * Gets the number of armies a player holds
+     * @return d_numArmies Number of armies of a player
+     */
+    public int getD_numArmies() {
+        return d_numArmies;
+    }
+
+    /**
+     * Sets the number of armies a player holds
+     * @param p_numArmies Number of armies of a player
+     */
+    public void setD_numArmies(int p_numArmies) {
+        this.d_numArmies = p_numArmies;
+    }
+
+    /**
+     * Gets the list of countries held by the player
+     * @return d_countryList List of Country objects under player's ownership
+     */
+    public List<Country> getD_countryList() {
+        return d_countryList;
+    }
+
+    /**
+     * Sets the list of countries held by the player
+     * @param p_countryList List of Country objects under player's ownership
+     */
+    public void setD_countryList(List<Country> p_countryList) {
+        this.d_countryList = p_countryList;
+    }
+
+    /**
+     * Gets the list of orders issued by the player
+     * @return d_orderList List of orders issued by the player
+     */
+    public List<Order> getD_orderList() {
+        return d_orderList;
+    }
+
+    /**
+     * Sets the list of orders issued by the player
+     * @param p_orderList List of orders issued by the player
+     */
+    public void setD_orderList(List<Order> p_orderList) {
+        this.d_orderList = p_orderList;
     }
 }
