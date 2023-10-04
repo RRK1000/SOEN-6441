7ffd8776c9e504338d47559f70dcc845a5c4cb71
MUSAIMAN <106506857+MUSAIMAN@users.noreply.github.com>
2023-10-03 23:14:29 -0400

    feat: Integrate the reinforcement func test into CommandParserTest

--- src/test/java/controller/CommandParserTest.java
+++ src/test/java/controller/CommandParserTest.java
@@ -1,20 +1,24 @@
 package controller;
 
-import models.Country;
-import models.Order;
-import models.Player;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.ArrayList;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import models.Country;
+import models.Map;
+import models.Order;
+import models.Player;
+import util.MapUtil;
 
 class CommandParserTest {
 	GameManager d_gameManager;
 	private Player d_player;
 	private Country d_country;
 	private Order d_order;
+	private Map d_map = new Map();
 
 	@BeforeEach
 	void setUp() {
@@ -23,15 +27,38 @@ class CommandParserTest {
 		d_country = new Country();
 		d_order = new Order(d_country, 3);
 		d_player.setD_currentOrder(d_order);
+
+		String player1Name = "Player1";
+		String player2Name = "Player2";
+
+		d_map = MapUtil.loadMap("src/test/resources/europe.map");
+
+		d_gameManager = new GameManager(d_map);
+		d_gameManager.addPlayer(player1Name);
+		d_gameManager.addPlayer(player2Name);
 	}
 
+	/**
+	 * This test tests the assignReinforcements function. It checks weather each
+	 * player is getting reinforcements according to war-zone rules. Expected
+	 * Results: return true if it matches the expected value.
+	 */
+	@Test
+	void assignReinforcements() {
+		int expectedPlayer1Armies = Math.max((d_map.getD_countryMapGraph().vertexSet().size() / 3), 3);
+		int expectedPlayer2Armies = Math.max((d_map.getD_countryMapGraph().vertexSet().size() / 3), 3);
+		d_gameManager.assignCountries(); // calls assignReinforcements internally
+
+		assertEquals(expectedPlayer1Armies, d_gameManager.getD_playerList().get(0).getD_numArmies());
+		assertEquals(expectedPlayer2Armies, d_gameManager.getD_playerList().get(1).getD_numArmies());
+	}
 
 	/**
 	 * Tests the scenario where a player attempts to deploy more armies than
-	 * available in their reinforcement pool.
-	 * The test first ensures that the player starts with a specific number of
-	 * armies. After issuing an order to deploy a certain number of armies, the test
-	 * checks that the player's remaining armies are as expected
+	 * available in their reinforcement pool. The test first ensures that the player
+	 * starts with a specific number of armies. After issuing an order to deploy a
+	 * certain number of armies, the test checks that the player's remaining armies
+	 * are as expected
 	 *
 	 * The test then tries to issue another order to deploy more armies than the
 	 * player has available. This should result in an IllegalArgumentException. The
