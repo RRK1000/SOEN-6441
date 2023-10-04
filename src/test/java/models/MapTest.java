17806ec0e90331b3bbf72b23afb3603584b37e76
Abhigyan Singh <singh.abhigyan2001@gmail.com>
2023-10-04 16:32:18 -0400

    Add: unit tests for models
    Add: unit tests for models

---
+++ src/test/java/models/MapTest.java
@@ -0,0 +1,82 @@
+package models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import models.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Test class for the Map class.
+ */
+class MapTest {
+    private Map map;
+
+    /**
+     * Sets up the test fixture before each test method.
+     */
+    @BeforeEach
+    void setUp() {
+        // Initialize the Map object or perform any necessary setup before each test.
+        map = new Map();
+    }
+
+    /**
+     * Test the getD_countryByID method of the Map class.
+     * Verifies that the correct country is retrieved by its ID.
+     */
+    @Test
+    void testGetD_countryByID() {
+        // Write test cases to test the getD_countryByID method.
+        // Use assertions to verify the expected behavior.
+    }
+
+    /**
+     * Test the getD_continentByID method of the Map class.
+     * Verifies that the correct continent is retrieved by its ID.
+     */
+    @Test
+    void testGetD_continentByID() {
+        // Write test cases to test the getD_continentByID method.
+        // Use assertions to verify the expected behavior.
+    }
+
+    /**
+     * Test the getD_countryMapGraph method of the Map class.
+     * Verifies that the correct directed graph representing countries is returned.
+     */
+    @Test
+    void testGetD_countryMapGraph() {
+        // Write test cases to test the getD_countryMapGraph method.
+        // Use assertions to verify the expected behavior.
+    }
+
+    /**
+     * Test the setD_countryMapGraph method of the Map class.
+     * Verifies that the directed graph representing countries is correctly set.
+     */
+    @Test
+    void testSetD_countryMapGraph() {
+        // Write test cases to test the setD_countryMapGraph method.
+        // Use assertions to verify the expected behavior.
+    }
+
+    /**
+     * Test the getD_continentMapGraph method of the Map class.
+     * Verifies that the correct directed graph representing continents is returned.
+     */
+    @Test
+    void testGetD_continentMapGraph() {
+        // Write test cases to test the getD_continentMapGraph method.
+        // Use assertions to verify the expected behavior.
+    }
+
+    /**
+     * Test the setD_continentMapGraph method of the Map class.
+     * Verifies that the directed graph representing continents is correctly set.
+     */
+    @Test
+    void testSetD_continentMapGraph() {
+        // Write test cases to test the setD_continentMapGraph method.
+        // Use assertions to verify the expected behavior.
+    }
+}
