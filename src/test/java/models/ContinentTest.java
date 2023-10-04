4cb895d33e27880013a4e9c7bebe9ee03e978f82
Abhigyan Singh <singh.abhigyan2001@gmail.com>
2023-10-04 16:32:18 -0400

    Add: unit tests for models
    Add: unit tests for models

---
+++ src/test/java/models/ContinentTest.java
@@ -0,0 +1,118 @@
+package models;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.junit.jupiter.api.Assertions.*;
+/**
+ * Test class for the Continent class.
+ */
+class ContinentTest {
+    private Continent continent;
+    /**
+     * Sets up the tests before each test method.
+     */
+    @BeforeEach
+    void setUp() {
+        // Initialize the Continent object or any necessary setup before each test.
+        continent = new Continent();
+    }
+    /**
+     * Tests the addCountry method of Continent class.
+     * Verifies that country is correctly added to the continent's country list.
+     */
+    @Test
+    void testAddCountry() {
+        // Write test cases to test the addCountry method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the removeCountry method of Continent class.
+     * Verifies that a country is correctly removed from continent's country list.
+     */
+    @Test
+    void testRemoveCountry() {
+        // Write test cases to test the removeCountry method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the getD_continentID method of Continent class.
+     * Verifies that the correct continent ID is returned back.
+     */
+    @Test
+    void testGetD_continentID() {
+        // Write test cases to test the getD_continentID method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the setD_continentID method of the Continent class.
+     * Verifies that the continent ID is correctly set.
+     */
+
+    @Test
+    void testSetD_continentID() {
+        // Write test cases to test the setD_continentID method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the getD_continentName method of Continent class.
+     * Verifies that the correct continent Name is returned back.
+     */
+
+    @Test
+    void testGetD_continentName() {
+        // Write test cases to test the getD_continentName method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the setD_continentName method of the Continent class.
+     * Verifies that the continent name is correctly set.
+     */
+
+    @Test
+    void testSetD_continentName() {
+        // Write test cases to test the setD_continentName method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the getD_continentValue method of Continent class.
+     * Verifies that the correct continent value is returned back.
+     */
+
+    @Test
+    void testGetD_continentValue() {
+        // Write test cases to test the getD_continentValue method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the setD_continentValue method of the Continent class.
+     * Verifies that the continent value is correctly set.
+     */
+
+    @Test
+    void testSetD_continentValue() {
+        // Write test cases to test the setD_continentValue method.
+        // Use assertions to verify the expected behavior.
+    }
+
+    /**
+     * Tests the getD_countryList method of Continent class.
+     * Verifies that the correct list is returned back.
+     */
+
+    @Test
+    void testGetD_countryList() {
+        // Write test cases to test the getD_countryList method.
+        // Use assertions to verify the expected behavior.
+    }
+    /**
+     * Tests the setD_countryList method of the Continent class.
+     * Verifies that the country is correctly set in the list.
+     */
+
+    @Test
+    void testSetD_countryList() {
+        // Write test cases to test the setD_countryList method.
+        // Use assertions to verify the expected behavior.
+    }
+
+}
