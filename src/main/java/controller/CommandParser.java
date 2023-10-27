75cccbdbd24b621d6fe0358de31b6d3ef4b0963c
Anuja-Somthankar <anujasom@gmail.com>
2023-10-27 19:28:05 -0400

    feat : Implementation of State Pattern
    feat : Implementation of State Pattern

--- src/main/java/controller/CommandParser.java
+++ src/main/java/controller/CommandParser.java
@@ -5,6 +5,10 @@ import global.Constants;
 import models.Country;
 import models.Map;
 import models.Player;
+import phases.InitMapPhase;
+import phases.IssueOrderPhase;
+import phases.Phase;
+import phases.StartupPhase;
 import util.CommandUtil;
 import util.MapUtil;
 
@@ -19,18 +23,7 @@ import util.MapUtil;
  * @author Yusuke
  */
 public class CommandParser {
-
-    /**
-     * Checks the initial phase of the game.
-     *
-     * @param p_gameManager Instance of Game Manager class
-     * @return Boolean true if current phase is initial phase or else return false.
-     */
-    public static Boolean isGamePhaseMapInit(GameManager p_gameManager) {
-        return p_gameManager.getD_gamePhase() == GamePhase.Map_Init;
-    }
-
-    private static void displayError() {
+    public static void displayError() {
         System.out.println(Constants.CMD_ERROR);
         System.out.println(Constants.HELP_MESSAGE);
     }
@@ -41,18 +34,14 @@ public class CommandParser {
      * @param p_gameManager {@link GameManager}
      */
     public static void displayInstructions(GameManager p_gameManager) {
-        switch (p_gameManager.getD_gamePhase()) {
-            case Map_Init:
-                System.out.println(Constants.MAP_INIT_HELP);
-                break;
-            case Game_Startup:
-                System.out.println(Constants.GAME_STARTUP_HELP);
-                break;
-            case IssueOrder:
-                System.out.println(Constants.ISSUE_ORDER_HELP);
-                break;
-            default:
-                System.out.println(Constants.IN_GAME_HELP);
+        if (p_gameManager.d_gamePhase.getClass().equals(InitMapPhase.class)) {
+            System.out.println(Constants.MAP_INIT_HELP);
+        } else if (p_gameManager.d_gamePhase.getClass().equals(StartupPhase.class)) {
+            System.out.println(Constants.GAME_STARTUP_HELP);
+        } else if (p_gameManager.d_gamePhase.getClass().equals(IssueOrderPhase.class)) {
+            System.out.println(Constants.ISSUE_ORDER_HELP);
+        } else {
+            System.out.println(Constants.IN_GAME_HELP);
         }
         System.out.println(Constants.GAME_EXIT);
     }
@@ -67,7 +56,7 @@ public class CommandParser {
     public static void inputParser(GameManager p_gameManager, String p_input) {
         //Breaks the input command around space.
         String[] l_cmdSplit = p_input.split(" ");
-        if (!CommandUtil.isValidCmd(p_input, p_gameManager.getD_gamePhase())) {
+        if (!CommandUtil.isValidCmd(p_input, p_gameManager.d_gamePhase)) {
             displayError();
             return;
         }
@@ -88,186 +77,56 @@ public class CommandParser {
                     System.out.println(Constants.HELP_MESSAGE);
                     break;
                 }
-
-                if (p_gameManager.getD_gamePhase() == GamePhase.Map_Init
-                        || p_gameManager.getD_gamePhase() == GamePhase.Game_Startup) {
-                    MapUtil.showMap(l_map);
-                } else if (p_gameManager.getD_gamePhase() == GamePhase.AssignReinforcements
-                        || p_gameManager.getD_gamePhase() == GamePhase.IssueOrder) {
-                    p_gameManager.showMap();
-                }
+                p_gameManager.d_gamePhase.showMap(l_map, p_gameManager);
                 break;
 
             case Commands.EDIT_NEIGHBOR:
 
                 String[] l_editNeighbourInput = p_input.split(" -");
-                for (int l_i = 1; l_i < l_editNeighbourInput.length; l_i++) {
-                    //Handles the command to add the neighbour country
-                    if (l_editNeighbourInput[l_i].startsWith("add")) {
-                        String[] l_addParams = l_editNeighbourInput[l_i].split(" ");
-                        //checks if add is followed by countryID and neighborcountryID
-
-                        int l_countryID = Integer.parseInt(l_addParams[1]);
-                        int l_neighbourID = Integer.parseInt(l_addParams[2]);
-                        System.out.println("adding neighbor country: " + l_neighbourID);
-                        //Calls addNeighbour() for adding the neighbour country
-                        MapUtil.addNeighbour(l_map, l_countryID, l_neighbourID);
-
-                        //Handles the command to remove the neighbour country
-                    } else if (l_editNeighbourInput[l_i].startsWith("remove")) {
-                        String[] l_removeParams = l_editNeighbourInput[l_i].split(" ");
-                        //checks if remove is followed by countryID and neighborcountryID
-
-                        int l_countryID = Integer.parseInt(l_removeParams[1]);
-                        int l_neighbourID = Integer.parseInt(l_removeParams[2]);
-                        System.out.println("removing neighbour country: " + l_neighbourID);
-                        //Calls removeNeighbour() for removing the neighbour country
-                        MapUtil.removeNeighbour(l_map, l_countryID, l_neighbourID);
-
-                    } else {
-                        System.out.println(Constants.CMD_ERROR);
-                    }
-                }
-
+                p_gameManager.d_gamePhase.editNeighbor(l_editNeighbourInput, l_map);
                 break;
 
             case Commands.EDIT_CONTINENT:
                 String[] l_inputSplit = p_input.split(" -");
-                for (int l_i = 1; l_i < l_inputSplit.length; l_i++) {
-                    //Handles the command to add continent
-                    if (l_inputSplit[l_i].startsWith("add")) {
-                        String[] l_addParams = l_inputSplit[l_i].split(" ");
-                        //checks if add is followed by continentID and continentvalue
-
-                        int l_continentID = Integer.parseInt(l_addParams[1]);
-                        int l_continentValue = Integer.parseInt(l_addParams[2]);
-                        System.out.println("adding continent: " + l_continentID);
-                        MapUtil.addContinent(l_map, l_continentID, l_continentValue);
-
-                        //Handles the command to remove the continent
-                    } else if (l_inputSplit[l_i].startsWith("remove")) {
-                        String[] l_removeParams = l_inputSplit[l_i].split(" ");
-                        //checks if remove is followed by continentID
-
-                        int l_continentID = Integer.parseInt(l_removeParams[1]);
-                        System.out.println("removing continent: " + l_continentID);
-                        MapUtil.removeContinent(l_map, l_continentID);
-
-                    } else {
-                        System.out.println(Constants.CMD_ERROR);
-                    }
-                }
-
+                p_gameManager.d_gamePhase.editContinent(l_inputSplit, l_map);
                 break;
 
             case Commands.EDIT_COUNTRY:
                 String[] l_editCountryInput = p_input.split(" -");
-                for (int l_i = 1; l_i < l_editCountryInput.length; l_i++) {
-                    //checks if add is followed by countryID and continentID
-                    if (l_editCountryInput[l_i].startsWith("add")) {
-                        String[] l_addParams = l_editCountryInput[l_i].split(" ");
-
-                        int l_countryID = Integer.parseInt(l_addParams[1]);
-                        int l_continentID = Integer.parseInt(l_addParams[2]);
-                        System.out.println("adding country: " + l_countryID);
-                        MapUtil.addCountry(l_map, l_countryID, l_continentID);
-
-                    } else if (l_editCountryInput[l_i].startsWith("remove")) {
-                        //checks if remove is followed by countryID
-                        String[] l_removeParams = l_editCountryInput[l_i].split(" ");
-
-                        int l_countryID = Integer.parseInt(l_removeParams[1]);
-                        System.out.println("removing country: " + l_countryID);
-                        MapUtil.removeCountry(l_map, l_countryID);
-
-                    } else {
-                        System.out.println(Constants.CMD_ERROR);
-                    }
-                }
+                p_gameManager.d_gamePhase.editCountry(l_editCountryInput, l_map);
                 break;
 
             case Commands.SAVE_MAP:
-                //Checks if the savemap command is followed by the filename
-                System.out.println("Saving the map");
-                //calls saveMap() function
-                Boolean l_isMapSaved = MapUtil.saveMap(l_map, l_cmdSplit[1]);
-                //Checks if the map was saved successfully
-                if (l_isMapSaved) {
-                    System.out.println("Map is saved");
-                    System.out.println("Load the map to start the game.");
-                } else {
-                    System.out.println("Map Invalid");
-                }
-
+                p_gameManager.d_gamePhase.saveMap(l_map, l_cmdSplit);
                 break;
 
             case Commands.EDIT_MAP:
-
-                //calls the editMap() function
-                p_gameManager.setD_map(MapUtil.editMap(l_cmdSplit[1]));
-                System.out.println("Map loaded to be edited...");
-
+                p_gameManager.d_gamePhase.editMap(p_gameManager, l_cmdSplit);
                 break;
 
             case Commands.VALIDATE_MAP:
-                if (l_map != null && isGamePhaseMapInit(p_gameManager)) {
-                    System.out.println("Validating the map");
-                    //Checks if the map is valid or not
-                    if (MapUtil.isValidMap(l_map)) {
-                        p_gameManager.setD_map(l_map);
-                    } else {
-                        System.out.println("Map validation unsuccessful");
-                    }
+                if (l_map != null) {
+                    p_gameManager.d_gamePhase.validateMap(l_map, p_gameManager);
                 } else {
                     displayError();
                 }
                 break;
 
             case Commands.LOAD_MAP:
-
-                //calls loadMap() function
-                l_map = MapUtil.loadMap(l_cmdSplit[1]);
-                if (!l_map.getD_countryMapGraph().vertexSet().isEmpty() && MapUtil.isValidMap(l_map)) {
-                    //Set the game phase to start up phase
-                    p_gameManager.setD_gamePhase(GamePhase.Game_Startup);
-                    p_gameManager.setD_map(l_map);
-
-                    System.out.println("Next, add players to the game");
-                } else {
-                    System.out.println("Map validation unsuccessful");
-                    break;
-                }
-                System.out.println(Constants.HELP_MESSAGE);
-
+                p_gameManager.d_gamePhase.loadMap(l_cmdSplit[1], p_gameManager);
                 break;
 
             case Commands.GAME_PLAYER:
-                p_gameManager.setD_gamePhase(GamePhase.Game_Startup);
                 if (p_gameManager.getD_map() == null) {
                     System.out.println("Map not loaded");
                     System.out.println(Constants.HELP_MESSAGE);
                     break;
                 }
-                //Handles the case to add or remove player
-                for (int l_i = 1; l_i < l_cmdSplit.length - 1; l_i++) {
-                    if (l_cmdSplit[l_i].startsWith("-add")
-                            && !l_cmdSplit[l_i + 1].startsWith("-")) {
-                        String l_playername = l_cmdSplit[l_i + 1];
-                        //calls addPlayer() to add the Player in the game
-                        p_gameManager.addPlayer(l_playername);
-                    } else if (l_cmdSplit[l_i].startsWith("-remove")
-                            && !l_cmdSplit[l_i + 1].startsWith("-")) {
-                        String l_playername = l_cmdSplit[l_i + 1];
-                        //calls removePlayer() to remove Player from the game
-                        p_gameManager.removePlayer(l_playername);
-                    }
-                }
+                p_gameManager.d_gamePhase.gamePlayer(l_cmdSplit, p_gameManager);
                 break;
 
             case Commands.ASSIGN_COUNTRIES:
-                //assign countries to the players
-                p_gameManager.setD_gamePhase(GamePhase.AssignReinforcements);
-                p_gameManager.assignCountries();
+                p_gameManager.d_gamePhase.assignCountries(p_gameManager);
                 break;
 
             case Commands.DEPLOY_ORDER:
@@ -280,7 +139,7 @@ public class CommandParser {
                 //Getting number of armies
                 int l_numArmies = Integer.parseInt(l_cmdSplit[2]);
                 //call issueOrder()
-                p_gameManager.issueOrder(l_country, l_numArmies);
+                p_gameManager.d_gamePhase.deploy(l_currentPlayer, l_country, l_numArmies);
 
                 //If the current player has zero armies, move to the next player.
                 if (l_currentPlayer.getD_numArmies() == 0) {
@@ -293,6 +152,7 @@ public class CommandParser {
                         p_gameManager.executeOrder();
                         //Assigning armies to the player
                         p_gameManager.assignReinforcements();
+                        p_gameManager.d_gamePhase = p_gameManager.d_gamePhase.nextPhase();
                     }
                     l_currentPlayer = p_gameManager.getD_playerList().get(p_gameManager.getD_currentPlayerTurn());
                     System.out.println("Player " + l_currentPlayer.getD_playerName() + "'s turn ");
---
+++ src/main/java/phases/StartupPhase.java
@@ -0,0 +1,84 @@
+package phases;
+
+import controller.CommandParser;
+import controller.GameManager;
+import models.Country;
+import models.Map;
+import models.Player;
+
+public class StartupPhase implements Phase {
+
+    @Override
+    public Phase nextPhase() {
+        return new IssueOrderPhase();
+    }
+
+    @Override
+    public void showMap(Map p_map, GameManager p_gameManager) {
+        p_gameManager.showMap();
+    }
+
+    @Override
+    public void deploy(Player p_currentPlayer, Country p_country, int p_num) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void editCountry(String[] p_editCountryInput, Map p_map) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void editContinent(String[] p_inputSplit, Map p_map) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void editNeighbor(String[] p_editNeighbourInput, Map p_map) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void validateMap(Map p_map, GameManager p_gameManager) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void saveMap(Map p_map, String[] l_cmdSplit) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void editMap(GameManager p_gameManager, String[] l_cmdSplit) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void loadMap(String p_fileName, GameManager p_gameManager) {
+        CommandParser.displayError();
+    }
+
+    @Override
+    public void gamePlayer(String[] p_cmdSplit, GameManager p_gameManager) {
+        //Handles the case to add or remove player
+        for (int l_i = 1; l_i < p_cmdSplit.length - 1; l_i++) {
+            if (p_cmdSplit[l_i].startsWith("-add")
+                    && !p_cmdSplit[l_i + 1].startsWith("-")) {
+                String l_playername = p_cmdSplit[l_i + 1];
+                //calls addPlayer() to add the Player in the game
+                p_gameManager.addPlayer(l_playername);
+            } else if (p_cmdSplit[l_i].startsWith("-remove")
+                    && !p_cmdSplit[l_i + 1].startsWith("-")) {
+                String l_playername = p_cmdSplit[l_i + 1];
+                //calls removePlayer() to remove Player from the game
+                p_gameManager.removePlayer(l_playername);
+            }
+        }
+    }
+
+    @Override
+    public void assignCountries(GameManager p_gameManager) {
+        p_gameManager.assignCountries();
+        p_gameManager.d_gamePhase = this.nextPhase();
+    }
+}
