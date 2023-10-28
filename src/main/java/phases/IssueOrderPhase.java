ab5299f0be14036e8b090bd58d635b0f467ab796
Anuja-Somthankar <anujasom@gmail.com>
2023-10-27 21:29:34 -0400

    refactor : Reformat code to follow convention
    refactor : Reformat code to follow convention

--- src/main/java/phases/IssueOrderPhase.java
+++ src/main/java/phases/IssueOrderPhase.java
@@ -1,6 +1,5 @@
 package phases;
 
-import controller.CommandParser;
 import controller.GameManager;
 import global.Constants;
 import models.Country;
@@ -11,6 +10,7 @@ import models.Player;
 public class IssueOrderPhase implements Phase {
     /**
      * This method shifts the game phase to the next phase.
+     *
      * @return Phase class
      */
     @Override
@@ -20,7 +20,8 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method displays the map.
-     * @param p_map The map object
+     *
+     * @param p_map         The map object
      * @param p_gameManager The object of the game manager.
      */
     @Override
@@ -57,8 +58,9 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_editCountryInput The input given by the user to add/remove countries
-     * @param p_map The Map object
+     * @param p_map              The Map object
      */
     @Override
     public void editCountry(String[] p_editCountryInput, Map p_map) {
@@ -67,8 +69,9 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_inputSplit The input given by the user to add/remove continents
-     * @param p_map The Map object
+     * @param p_map        The Map object
      */
     @Override
     public void editContinent(String[] p_inputSplit, Map p_map) {
@@ -77,8 +80,9 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_editNeighbourInput The input given by the user to add/remove neighbours
-     * @param p_map The Map object
+     * @param p_map                The Map object
      */
     @Override
     public void editNeighbor(String[] p_editNeighbourInput, Map p_map) {
@@ -87,7 +91,8 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
-     * @param p_map The Map object
+     *
+     * @param p_map         The Map object
      * @param p_gameManager The GameManager object
      */
     @Override
@@ -97,7 +102,8 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
-     * @param p_map The Map object
+     *
+     * @param p_map      The Map object
      * @param l_cmdSplit The input given by the user to save map
      */
     @Override
@@ -107,8 +113,9 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_gameManager The game manager object
-     * @param l_cmdSplit The input given by the user
+     * @param l_cmdSplit    The input given by the user
      */
     @Override
     public void editMap(GameManager p_gameManager, String[] l_cmdSplit) {
@@ -117,7 +124,8 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
-     * @param p_fileName The name of the file from where the map is to be loaded
+     *
+     * @param p_fileName    The name of the file from where the map is to be loaded
      * @param p_gameManager The game manager object
      */
     @Override
@@ -127,7 +135,8 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
-     * @param p_cmdSplit The input given by the user to add/remove players
+     *
+     * @param p_cmdSplit    The input given by the user to add/remove players
      * @param p_gameManager The game manager object
      */
     @Override
@@ -137,6 +146,7 @@ public class IssueOrderPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_gameManager The game manager object
      */
     @Override
--- src/main/java/phases/StartupPhase.java
+++ src/main/java/phases/StartupPhase.java
@@ -1,6 +1,5 @@
 package phases;
 
-import controller.CommandParser;
 import controller.GameManager;
 import global.Constants;
 import models.Country;
@@ -11,6 +10,7 @@ public class StartupPhase implements Phase {
 
     /**
      * This method shifts the game phase to the next phase.
+     *
      * @return Phase class
      */
     @Override
@@ -20,7 +20,8 @@ public class StartupPhase implements Phase {
 
     /**
      * This method displays the map.
-     * @param p_map The map object
+     *
+     * @param p_map         The map object
      * @param p_gameManager The object of the game manager.
      */
     @Override
@@ -30,6 +31,7 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_currentPlayer The current player
      * @param p_country       The country in the order
      * @param p_num           The number of armies to be deployed
@@ -41,8 +43,9 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_editCountryInput The input given by the user to add/remove countries
-     * @param p_map The Map object
+     * @param p_map              The Map object
      */
     @Override
     public void editCountry(String[] p_editCountryInput, Map p_map) {
@@ -51,8 +54,9 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_inputSplit The input given by the user to add/remove continents
-     * @param p_map The Map object
+     * @param p_map        The Map object
      */
     @Override
     public void editContinent(String[] p_inputSplit, Map p_map) {
@@ -61,8 +65,9 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_editNeighbourInput The input given by the user to add/remove neighbours
-     * @param p_map The Map object
+     * @param p_map                The Map object
      */
     @Override
     public void editNeighbor(String[] p_editNeighbourInput, Map p_map) {
@@ -71,7 +76,8 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
-     * @param p_map The Map object
+     *
+     * @param p_map         The Map object
      * @param p_gameManager The GameManager object
      */
     @Override
@@ -81,7 +87,8 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
-     * @param p_map The Map object
+     *
+     * @param p_map      The Map object
      * @param l_cmdSplit The input given by the user to save map
      */
     @Override
@@ -91,8 +98,9 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
+     *
      * @param p_gameManager The game manager object
-     * @param l_cmdSplit The input given by the user
+     * @param l_cmdSplit    The input given by the user
      */
     @Override
     public void editMap(GameManager p_gameManager, String[] l_cmdSplit) {
@@ -101,7 +109,8 @@ public class StartupPhase implements Phase {
 
     /**
      * This method prints the invalid phase error as it is not a valid command for this phase.
-     * @param p_fileName The name of the file from where the map is to be loaded
+     *
+     * @param p_fileName    The name of the file from where the map is to be loaded
      * @param p_gameManager The game manager object
      */
     @Override
@@ -111,7 +120,8 @@ public class StartupPhase implements Phase {
 
     /**
      * This method adds/removes the game players
-     * @param p_cmdSplit The input given by the user to add/remove players
+     *
+     * @param p_cmdSplit    The input given by the user to add/remove players
      * @param p_gameManager The game manager object
      */
     @Override
@@ -134,6 +144,7 @@ public class StartupPhase implements Phase {
 
     /**
      * This method is used to assign countries
+     *
      * @param p_gameManager The game manager object
      */
     @Override
