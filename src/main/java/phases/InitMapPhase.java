e22f2cef81c88d0c06dd9c902069b64d8861611b
Anuja-Somthankar <anujasom@gmail.com>
2023-10-27 21:11:37 -0400

    docs : javadoc added for game phase files
    docs : javadoc added for game phase files

--- src/main/java/phases/InitMapPhase.java
+++ src/main/java/phases/InitMapPhase.java
@@ -9,16 +9,31 @@ import models.Player;
 import util.MapUtil;
 
 public class InitMapPhase implements Phase {
+    /**
+     * This method shifts the game phase to the next phase.
+     * @return Phase class
+     */
     @Override
     public Phase nextPhase() {
         return new StartupPhase();
     }
 
+    /**
+     * This method prints the invalid phase error as it is not a valid command for this phase.
+     * @param p_currentPlayer The current player
+     * @param p_country       The country in the order
+     * @param p_num           The number of armies to be deployed
+     */
     @Override
     public void deploy(Player p_currentPlayer, Country p_country, int p_num) {
         System.out.println(Constants.INVALID_PHASE_ERROR);
     }
 
+    /**
+     * This method adds and removes countries during MapInit phase.
+     * @param p_editCountryInput The input given by the user to add/remove countries
+     * @param p_map The Map object
+     */
     @Override
     public void editCountry(String[] p_editCountryInput, Map p_map) {
         for (int l_i = 1; l_i < p_editCountryInput.length; l_i++) {
@@ -45,6 +60,11 @@ public class InitMapPhase implements Phase {
         }
     }
 
+    /**
+     * This method adds and removes continents during MapInit phase.
+     * @param p_inputSplit The input given by the user to add/remove continents
+     * @param p_map The Map object
+     */
     @Override
     public void editContinent(String[] p_inputSplit, Map p_map) {
         for (int l_i = 1; l_i < p_inputSplit.length; l_i++) {
@@ -73,6 +93,11 @@ public class InitMapPhase implements Phase {
         }
     }
 
+    /**
+     * This method adds and removes neighbour during MapInit phase.
+     * @param p_editNeighbourInput The input given by the user to add/remove neighbours
+     * @param p_map The Map object
+     */
     @Override
     public void editNeighbor(String[] p_editNeighbourInput, Map p_map) {
         for (int l_i = 1; l_i < p_editNeighbourInput.length; l_i++) {
@@ -104,6 +129,11 @@ public class InitMapPhase implements Phase {
         }
     }
 
+    /**
+     * This method is used to validate the map.
+     * @param p_map The Map object
+     * @param p_gameManager The GameManager object
+     */
     @Override
     public void validateMap(Map p_map, GameManager p_gameManager) {
         //Checks if the map is valid or not
@@ -114,6 +144,11 @@ public class InitMapPhase implements Phase {
         }
     }
 
+    /**
+     * This method is used to save the map
+     * @param p_map The Map object
+     * @param l_cmdSplit The input given by the user to save map
+     */
     @Override
     public void saveMap(Map p_map, String[] l_cmdSplit) {
         //Checks if the map was saved successfully
@@ -125,17 +160,32 @@ public class InitMapPhase implements Phase {
         }
     }
 
+    /**
+     * This method is used to edit the map
+     * @param p_gameManager The game manager object
+     * @param l_cmdSplit The input given by the user
+     */
     @Override
     public void editMap(GameManager p_gameManager, String[] l_cmdSplit) {
         p_gameManager.setD_map(MapUtil.editMap(l_cmdSplit[1]));
         System.out.println("Map loaded to be edited...");
     }
 
+    /**
+     * This method displays the map.
+     * @param p_map The map object
+     * @param p_gameManager The object of the game manager.
+     */
     @Override
     public void showMap(Map p_map, GameManager p_gameManager) {
         MapUtil.showMap(p_map);
     }
 
+    /**
+     * This method loads the map
+     * @param p_fileName The name of the file from where the map is to be loaded
+     * @param p_gameManager The game manager object
+     */
     @Override
     public void loadMap(String p_fileName, GameManager p_gameManager) {
         Map l_map = MapUtil.loadMap(p_fileName);
@@ -152,11 +202,20 @@ public class InitMapPhase implements Phase {
         System.out.println(Constants.HELP_MESSAGE);
     }
 
+    /**
+     * This method prints the invalid phase error as it is not a valid command for this phase.
+     * @param p_cmdSplit The input given by the user to add/remove players
+     * @param p_gameManager The game manager object
+     */
     @Override
     public void gamePlayer(String[] p_cmdSplit, GameManager p_gameManager) {
         System.out.println(Constants.INVALID_PHASE_ERROR);
     }
 
+    /**
+     * This method prints the invalid phase error as it is not a valid command for this phase.
+     * @param p_gameManager The game manager object
+     */
     @Override
     public void assignCountries(GameManager p_gameManager) {
         System.out.println(Constants.INVALID_PHASE_ERROR);
