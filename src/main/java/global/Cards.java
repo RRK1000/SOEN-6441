dd65709a36584dae5831434b8d9288cd5b62e635
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-10-30 15:02:08 -0400

    feat: add card constants

---
+++ src/main/java/global/Cards.java
@@ -0,0 +1,12 @@
+package global;
+
+/**
+ * Represents the available cards types
+ */
+public class Cards {
+    public static final String BOMB_CARD = "bomb";
+    public static final String BLOCKADE_CARD = "blockade";
+    public static final String AIRLIFT_CARD = "airlift";
+    public static final String DIPLOMACY_CARD = "diplomacy";
+
+}
