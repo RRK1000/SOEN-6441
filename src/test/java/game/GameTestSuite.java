24e7aabc27cfa2bc10364e2873b1dd153db329d7
Anuja-Somthankar <anujasom@gmail.com>
2023-11-05 18:58:18 -0500

    test : test suite for game package
    test : test suite for game package

---
+++ src/test/java/game/GameTestSuite.java
@@ -0,0 +1,10 @@
+package game;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({GameEngineTest.class})
+public class GameTestSuite {
+
+}
