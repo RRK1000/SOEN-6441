2b6e2d60f614c8db1dde37d6d23988eb481b6373
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-11-08 22:24:31 -0500

    docs: add missing javadoc

--- src/main/java/global/Cards.java
+++ src/main/java/global/Cards.java
@@ -6,9 +6,21 @@ package global;
  * @author Rishi Ravikumar
  */
 public class Cards {
+    /**
+     * Bomb Card Constant
+     */
     public static final String BOMB_CARD = "bomb-card";
+    /**
+     * Blockade Card Constant
+     */
     public static final String BLOCKADE_CARD = "blockade-card";
+    /**
+     * Airlift Card Constant
+     */
     public static final String AIRLIFT_CARD = "airlift-card";
+    /**
+     * Diplomacy Card Constant
+     */
     public static final String DIPLOMACY_CARD = "diplomacy-card";
 
 }
