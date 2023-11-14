a51edfc2151cb7d6f3cbbc1039ae2217ee60e434
Anuja-Somthankar <anujasom@gmail.com>
2023-11-14 16:45:31 -0500

    feat: Strategy Pattern
    feat: Strategy Pattern

---
+++ src/main/java/strategy/HumanStrategy.java
@@ -0,0 +1,18 @@
+package strategy;
+
+import controller.GameManager;
+import models.Order;
+
+public class HumanStrategy implements Strategy{
+
+    /**
+     * Issues an order according to player strategy
+     *
+     * @param p_gameManager
+     * @return
+     */
+    @Override
+    public Order createOrder(GameManager p_gameManager) {
+        return null;
+    }
+}
