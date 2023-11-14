b86966f45018e604e3b251c2e259a30a74860f86
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-11-14 18:48:23 -0500

    test: add BenevolentStrategyTest.java

--- src/test/java/strategy/BenevolentStrategyTest.java
+++ src/test/java/strategy/BenevolentStrategyTest.java
@@ -1,7 +1,6 @@
 package strategy;
 
 import controller.GameManager;
-import models.Country;
 import models.Order;
 import models.Player;
 import orders.AdvanceOrder;
@@ -9,7 +8,9 @@ import orders.DeployOrder;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import phases.ExecuteOrderPhase;
 import phases.InitMapPhase;
+import phases.IssueOrderPhase;
 import phases.Phase;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,10 +32,11 @@ class BenevolentStrategyTest {
         d_gameManager = new GameManager();
         d_gamePhase = new InitMapPhase();
 
-        d_gamePhase.loadMap("validMap2.txt", d_gameManager);
+        d_gamePhase.loadMap("europe.map", d_gameManager);
         String l_player1Name = "Player1";
         String l_player2Name = "Player2";
         d_gameManager.addPlayer(l_player1Name);
+        d_gameManager.findPlayerByName(l_player1Name).setD_playerStrategy(new BenevolentStrategy());
         d_gameManager.addPlayer(l_player2Name);
         d_gameManager.getD_gamePhase().assignCountries(d_gameManager);
     }
@@ -48,20 +50,40 @@ class BenevolentStrategyTest {
     }
 
     @Test
-    void issueOrderTest() {
-        Player l_p1 = d_gameManager.getD_playerList().get(0);
-        Country l_countryFrom = l_p1.getD_countryList().get(0);
-        Order l_deployOrder1 = new DeployOrder(l_p1, l_countryFrom, 6);
-        l_deployOrder1.execute();
+    void createOrderTest1() {
+        Order l_order = d_gameManager.findPlayerByName("Player1")
+                .getD_playerStrategy().createOrder(d_gameManager);
+        assertTrue(l_order instanceof DeployOrder);
+    }
 
-        Player l_p2 = d_gameManager.getD_playerList().get(1);
-        Country l_countryTo = l_p2.getD_countryList().get(0);
-        Order l_deployOrder2 = new DeployOrder(l_p2, l_countryTo, 1);
-        l_deployOrder2.execute();
+    @Test
+    void createOrderTest2() {
+        Player l_currentPlayer = d_gameManager.findPlayerByName("Player1");
+        Order l_order = d_gameManager.findPlayerByName("Player1")
+                .getD_playerStrategy().createOrder(d_gameManager);
+        l_currentPlayer.setD_currentOrder(l_order);
+        l_currentPlayer.issueOrder();
 
-        Order l_advanceOrder = new AdvanceOrder(l_p1, l_countryFrom, l_countryTo, 5);
-        l_advanceOrder.execute();
-        assertSame(l_countryTo.getD_owner(), l_p1);
-        assertFalse(l_p1.getD_playerCardList().isEmpty());
+        Order l_order2 = d_gameManager.findPlayerByName("Player1")
+                .getD_playerStrategy().createOrder(d_gameManager);
+
+        assertFalse(l_order2 instanceof DeployOrder);
+    }
+
+    @Test
+    void createOrderTest3() {
+        Player l_currentPlayer = d_gameManager.findPlayerByName("Player1");
+        Order l_order = d_gameManager.findPlayerByName("Player1")
+                .getD_playerStrategy().createOrder(d_gameManager);
+        l_currentPlayer.setD_currentOrder(l_order);
+        l_currentPlayer.issueOrder();
+        d_gameManager.setD_gamePhase(new ExecuteOrderPhase());
+        d_gameManager.getD_gamePhase().executeOrder(d_gameManager);
+        d_gameManager.setD_gamePhase(new IssueOrderPhase());
+
+        l_currentPlayer.setD_numArmies(0);
+        l_order = l_currentPlayer.getD_playerStrategy().createOrder(d_gameManager);
+        l_currentPlayer.setD_currentOrder(l_order);
+        assertTrue(l_order instanceof AdvanceOrder);
     }
 }
