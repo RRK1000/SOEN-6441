efd28cbcaae4c5b7e5f0696bc0efda6c5ef46dfc
Nimisha Jadav <nimisha.jadav7@gmail.com>
2023-11-15 15:55:27 -0500

    refac: updated methods where “p_arg” param was not used
    refac: updated methods where “p_arg” param was not used

--- src/main/java/gamelog/Observer.java
+++ src/main/java/gamelog/Observer.java
@@ -10,7 +10,6 @@ public interface Observer {
      * Updates observers
      *
      * @param p_observable observable object
-     * @param p_arg        Argument object
      */
-    void update(Observable p_observable, Object p_arg);
+    void update(Observable p_observable);
 }
