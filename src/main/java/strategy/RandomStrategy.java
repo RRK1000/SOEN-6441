eb9e8f024b9c3aaa5132fbf7b9b1353da2d482c4
Anuja-Somthankar <anujasom@gmail.com>
2023-11-16 18:32:51 -0500

    feat: RandomStrategy
    feat: RandomStrategy

--- src/main/java/strategy/RandomStrategy.java
+++ src/main/java/strategy/RandomStrategy.java
@@ -3,11 +3,13 @@ package strategy;
 import controller.GameManager;
 import models.Order;
 import models.Player;
+import orders.AdvanceOrder;
 import orders.DeployOrder;
 
+import java.util.List;
 import java.util.Random;
 
-public class RandomStrategy implements Strategy{
+public class RandomStrategy implements Strategy {
 
     /**
      * Creates an order according to player strategy
@@ -23,14 +25,29 @@ public class RandomStrategy implements Strategy{
         int l_numArmies = l_currentPlayer.getD_numArmies();
         int l_randNumArmies = 1;
         Order l_order;
-        if(l_numArmies != 0) {
+        if (l_numArmies != 0) {
             int l_randCountry = l_random.nextInt(l_currentPlayer.getD_countryList().size());
-            l_randNumArmies = Math.max(1, l_random.nextInt(l_numArmies+1));
-            l_order = new DeployOrder(l_currentPlayer, l_currentPlayer.getD_countryList().get(l_randCountry-1), l_randNumArmies);
+            l_randNumArmies = Math.max(1, l_random.nextInt(l_numArmies + 1));
+            l_order = new DeployOrder(l_currentPlayer, l_currentPlayer.getD_countryList().get(l_randCountry), l_randNumArmies);
             return l_order;
-        }else {
+        } else {
             //Advance
+            int l_tryAdvanceOrders = Math.max(1, l_currentPlayer.getD_countryList().size());
+            int l_randCountryFrom = -1;
+            for (int l_i = 1; l_i < l_tryAdvanceOrders; l_i++) {
+                l_randCountryFrom = l_random.nextInt(l_currentPlayer.getD_countryList().size());
+                if (l_currentPlayer.getD_countryList().get(l_randCountryFrom).getD_numArmies() != 0) {
+                    break;
+                }
+            }
+            if (l_randCountryFrom == -1)
+                return null;
+            int l_randAttackArmies = Math.max(1, l_random.nextInt(l_currentPlayer.getD_countryList().get(l_randCountryFrom).getD_numArmies()));
+            List<Integer> l_neighborList = l_currentPlayer.getD_countryList().get(l_randCountryFrom).getD_neighbourCountryIDList();
+            int l_randCountryTo = l_random.nextInt(l_neighborList.size());
+            l_order = new AdvanceOrder(l_currentPlayer, l_currentPlayer.getD_countryList().get(l_randCountryFrom),
+                    p_gameManager.getD_map().getD_countryByID(l_neighborList.get(l_randCountryTo)), l_randAttackArmies);
+            return l_order;
         }
-        return null;
     }
 }
