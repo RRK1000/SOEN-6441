2e251c2ad93db2cdd5ff6574f5b2399a8f62076b
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-11-16 19:30:12 -0500

    feat: add cheater strategy

--- src/test/java/strategy/StrategyTestSuite.java
+++ src/test/java/strategy/StrategyTestSuite.java
@@ -4,6 +4,6 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({BenevolentStrategyTest.class, AggressiveStrategyTest.class})
+@SelectClasses({BenevolentStrategyTest.class, AggressiveStrategyTest.class, RandomStrategyTest.class, CheaterStrategy.class})
 public class StrategyTestSuite {
 }
