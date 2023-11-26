ac5e1802ef9330fde217326d77e520c32f271280
MUSAIMAN <106506857+MUSAIMAN@users.noreply.github.com>
2023-11-26 03:09:44 -0500

    refactor: change a file path to be read in the conquest class

--- src/test/java/util/ConquestMapFileReaderAdapterTest.java
+++ src/test/java/util/ConquestMapFileReaderAdapterTest.java
@@ -36,7 +36,7 @@ public class ConquestMapFileReaderAdapterTest {
 	@Test
 	void testLoadMapValidFile() {
 		try {
-			Map l_map = d_adapter.loadMap("ConquestTestMap.txt");
+			Map l_map = d_adapter.loadMap("conquestTestMap.txt");
 			assertNotNull(l_map, "Map should not be null for a valid file");
 		} catch (IOException e) {
 			fail("IOException should not be thrown for a valid file");
