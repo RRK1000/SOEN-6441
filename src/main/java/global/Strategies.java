03eae03f9f51e9306405cd1089374039c61cdc49
Rishi Ravikumar <rishiravi.k98@gmail.com>
2023-11-25 20:02:50 -0500

    docs: add documentation

--- src/main/java/global/Strategies.java
+++ src/main/java/global/Strategies.java
@@ -2,23 +2,23 @@ package global;
 
 public class Strategies {
     /**
-     * Bomb Card Constant
+     * Human Strategy Constant
      */
     public static final String HUMAN_STRATEGY = "Human";
     /**
-     * Blockade Card Constant
+     * Benevolent Strategy Constant
      */
     public static final String BENEVOLENT_STRATEGY = "Benevolent";
     /**
-     * Airlift Card Constant
+     * Aggressive Strategy Constant
      */
     public static final String AGGRESSIVE_STRATEGY = "Aggressive";
     /**
-     * Diplomacy Card Constant
+     * Random Strategy Constant
      */
     public static final String RANDOM_STRATEGY = "Random";
     /**
-     * Diplomacy Card Constant
+     * Cheater Strategy Constant
      */
     public static final String CHEATER_STRATEGY = "Cheater";
 }
