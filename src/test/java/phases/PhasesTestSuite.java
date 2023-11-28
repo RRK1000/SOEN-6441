84b92ca10bcfdca28c7e633281d3b6eda2cd6cd1
Nimisha Jadav <nimisha.jadav7@gmail.com>
2023-11-28 18:37:13 -0500

    test: abstract Phase class
    test: abstract Phase class

--- src/test/java/phases/PhasesTestSuite.java
+++ src/test/java/phases/PhasesTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({InitMapPhaseTest.class, IssueOrderPhaseTest.class, StartupPhaseTest.class, ExecuteOrderPhaseTest.class})
+@SelectClasses({InitMapPhaseTest.class, IssueOrderPhaseTest.class, StartupPhaseTest.class, ExecuteOrderPhaseTest.class, Phase.class})
 public class PhasesTestSuite {
 
 }
