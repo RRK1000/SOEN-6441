2aba75e48555bffc5f4c9266ce2d1908274e452b
Nimisha Jadav <nimisha.jadav7@gmail.com>
2023-11-28 20:38:19 -0500

    test: editneighbour method from InitMapPhase class
    test: editneighbour method from InitMapPhase class

--- src/test/java/phases/InitMapPhaseTest.java
+++ src/test/java/phases/InitMapPhaseTest.java
@@ -1,11 +1,15 @@
 package phases;
 
 import controller.GameManager;
+import models.Continent;
+import models.Country;
+import models.Map;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import util.MapUtil;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Tests to check the map initialisation phase
@@ -40,6 +44,20 @@ class InitMapPhaseTest {
     void loadMapTest(){
         d_gamePhase.loadMap("europe.map", d_gameManager);
         assertEquals(d_gameManager.getD_gamePhase().getClass(), StartupPhase.class);
+    }
+
+    /**
+     * Test case for editNeighbour method to verify the functionality of editing neighbours
+     * by adding a neighbouring relationship between two countries.
+     */
+    @Test
+    void testEditNeighbour(){
+        String[] input = {"editneighbor", "add 1 2"};
+        Map l_map = new Map();
+        MapUtil.addCountry(l_map,1,1);
+        MapUtil.addCountry(l_map,2,1);
+        d_gamePhase.editNeighbor(input,l_map);
+        assertTrue(l_map.getD_countryByID(1).getD_neighbourCountryIDList().contains(2));
     }
 
 }
